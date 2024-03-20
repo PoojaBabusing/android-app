45c03970fc90cbe475f26fb4140fe09fb0847f42
Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>
2024-03-20 12:02:44 +0530

    upgrade JVM verion

--- app/src/main/java/com/example/test_app/MainActivity.kt
+++ app/src/main/java/com/example/test_app/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.example.test_app
-
-import Multiply
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mylibrary.Multiply
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        System.out.println(Multiply.multiply(6, 2));
-        Multiply.printTheMessage("sdfghjkl")
+        Multiply.multiply(9, 2)
+        Multiply.printTheMessage("wertyuiop")
     }
 }
